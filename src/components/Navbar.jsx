c232987ee951c069d1384a67d6ce8de29fff6062
Larocca <Larocca@RIVPRO1.net>
2023-04-01 13:49:51 -0400

    Updated all photo paths to the public img folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import StarWarsLogo from '../../public/img/StarWars-logo.png';
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1920px-Star_wars2.svg.png" />    
+            {/* <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Star_wars2.svg/1920px-Star_wars2.svg.png" />  */}
+            <img src={StarWarsLogo} />       
         </nav>
     )
 }
